8b4ad2df32d6782a9fbe215a8555a166faba2939
mhasanfuadd22 <76463319+mhasanfuadd22@users.noreply.github.com>
2021-01-16 03:18:32 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1161370&s3=xnxx&click_id=MACMOOS&j1=1&j3=1", // Yang ini jangan dihapus, sebagai default
+    "IN" => "https://www.youtube.com/",
+    "ID" => "https://www.google.com/",
+);
